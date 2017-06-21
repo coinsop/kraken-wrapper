962a128032ba217b986b2556287c3a1b617ba813
Olivers De Abreu <oliversd@gmail.com>
2017-06-20 20:15:32 -0400

    Pass methods to Promises

--- src/Kraken.js
+++ src/Kraken.js
@@ -20,20 +20,21 @@ class Kraken {
    * Returns a json object with the unix and rfc 1123 time
    * of the Kraken servers
    * This is to aid in approximating the skew time between the server and client.
-   * @param  {Function} cb - callback]
    * @return {Object}  - JSON Object { unixtime: 1497805936,
    *                                                         rfc1123: 'Sun, 18 Jun 17 17:12:16 +0000' }
    */
-  getTime(cb) {
-    const options = {
-      hostname: this.__apiBase,
-      port: 443,
-      path: '/0/public/Time',
-      method: 'GET',
-    }
-    request(options).then((response) => {
-      return cb(null, response.result);
-    }).catch((error) => cb(error));
+  getTime() {
+    return new Promise((resolve, reject) => {
+      const options = {
+        hostname: this.__apiBase,
+        port: 443,
+        path: '/0/public/Time',
+        method: 'GET',
+      }
+      request(options).then((response) => {
+        resolve(response.result);
+      }).catch((error) => reject(error));
+    });
   }
 
   /**
@@ -42,31 +43,32 @@ class Kraken {
    * @param {string} [assets] - comma delimited list of assets to get
    *                                              info on for given asset class (optional).
    *                                              default = null
-   * @param  {Function} cb - callback]
    * @return {Object}  - JSON Object - { XETH: { aclass: 'currency', altname: 'ETH', decimals: 10,  display_decimals: 5 } }
    *
    */
-  getAssetInfo(assets=null, cb) {
-    let path = '/0/public/Assets';
+  getAssetInfo(assets=null) {
+    return new Promise((resolve, reject) => {
+      let path = '/0/public/Assets';
 
-    if (assets && assets !== 'all' ) {
-      if (typeof assets !== 'string') {
-        return cb('Assets option must be a string, could be all for all assets or a comma separated values such as ETH,XRP');
+      if (assets && assets !== 'all' ) {
+        if (typeof assets !== 'string') {
+          reject('Assets option must be a string, could be all for all assets or a comma separated values such as ETH,XRP'); //return cb('Assets option must be a string, could be all for all assets or a comma separated values such as ETH,XRP');
+        }
+        // Remove any whitespace
+        assets = assets.replace(/\s/g,'')
+        path = `${path}?asset=${assets}`;
       }
-      // Remove any whitespace
-      assets = assets.replace(/\s/g,'')
-      path = `${path}?asset=${assets}`;
-    }
 
-    const options = {
-      hostname: this.__apiBase,
-      port: 443,
-      path: path,
-      method: 'GET',
-    }
-    request(options).then((response) => {
-      return cb(null, response.result);
-    }).catch((error) => cb(error));
+      const options = {
+        hostname: this.__apiBase,
+        port: 443,
+        path: path,
+        method: 'GET'
+      }
+      request(options).then((response) => {
+        resolve(response.result); //return cb(null, response.result);
+      }).catch((error) => reject(error));
+    });
   }
 
   /**
@@ -76,53 +78,54 @@ class Kraken {
    * @param {object} [inputs] - { info: 'all' // (default = all)  leverage, fees, margin (optional)
    *                                                   pair: 'all' // (default = all), comma delimited list of asset pairs to get info on
    *                                                 }
-   * @param  {Function} cb - callback
    * @return {Object}  - JSON Object - "DASHUSD": {"altname": "DASHUSD","aclass_base": "currency","base": "DASH",
    *                                                                               "aclass_quote": "currency","quote": "ZUSD","lot": "unit","pair_decimals": 5,
    *                                                                               "lot_decimals": 8,"lot_multiplier": 1,"leverage_buy": [],"leverage_sell": [],"fees": [...],
    *                                                                               "fees_maker": [..],"fee_volume_currency": "ZUSD","margin_call": 80,"margin_stop": 40}
    *
    */
-  getTradableAssetPairs(inputs={}, cb) {
-    let path = '/0/public/AssetPairs';
-    let infoEnum = ['all', 'leverage', 'fees', 'margin'];
+  getTradableAssetPairs(inputs={}) {
+    return new Promise((resolve, reject) => {
+      let path = '/0/public/AssetPairs';
+      let infoEnum = ['all', 'leverage', 'fees', 'margin'];
 
-    if (inputs) {
-      let pairSeparator = '?';
+      if (inputs) {
+        let pairSeparator = '?';
 
-      // Check if inputs.info is valid
-      if (inputs.info && typeof inputs.info === 'string' && infoEnum.indexOf(inputs.info) >= 0) {
-        if (inputs.info !== 'all') { // although all is in the documentation of the Kraken API this value produces an error if used
-          path = `${path}?info=${inputs.info}`;
-          pairSeparator = '&';
+        // Check if inputs.info is valid
+        if (inputs.info && typeof inputs.info === 'string' && infoEnum.indexOf(inputs.info) >= 0) {
+          if (inputs.info !== 'all') { // although all is in the documentation of the Kraken API this value produces an error if used
+            path = `${path}?info=${inputs.info}`;
+            pairSeparator = '&';
+          }
+        } else {
+          reject('Option info must be a string, could be all, leverage, fees or margin');
+        }
+
+        // Check if inputs.pair is valid
+        if (inputs.pair && typeof inputs.pair === 'string') {
+          const pair = inputs.pair.replace(/\s/g,''); // Remove any whitespace
+          if (inputs.pair !== 'all') { // although all is in the documentation of the Kraken API this value produces an error if used
+            path = `${path}${pairSeparator}pair=${inputs.pair}`;
+          }
+        } else {
+          reject('pair option must be a string, could be all for all assets or a comma separated values such as ETH,XRP');
         }
       } else {
-        return cb('Option info must be a string, could be all, leverage, fees or margin');
+        reject("getTradableAssetPairs require an input {info:'all', pair:'all'} as first argument");
       }
 
-      // Check if inputs.pair is valid
-      if (inputs.pair && typeof inputs.pair === 'string') {
-        const pair = inputs.pair.replace(/\s/g,''); // Remove any whitespace
-        if (inputs.pair !== 'all') { // although all is in the documentation of the Kraken API this value produces an error if used
-          path = `${path}${pairSeparator}pair=${inputs.pair}`;
-        }
-      } else {
-        return cb('Assets option must be a string, could be all for all assets or a comma separated values such as ETH,XRP');
+      const options = {
+        hostname: this.__apiBase,
+        port: 443,
+        path: path,
+        method: 'GET',
       }
-    } else {
-      return cb("getTradableAssetPairs require an input {info:'all', pair:'all'} as first argument");
-    }
 
-    const options = {
-      hostname: this.__apiBase,
-      port: 443,
-      path: path,
-      method: 'GET',
-    }
-
-    request(options).then((response) => {
-      return cb(null, response.result);
-    }).catch((error) => cb(error));
+      request(options).then((response) => {
+        resolve(response.result);
+      }).catch((error) => reject(error));
+    });
   }
 
 }
--- test/kraken-test.js
+++ test/kraken-test.js
@@ -10,59 +10,61 @@ const kraken = new Kraken();
 describe('Kraken', () => {
   describe('getTime', () => {
     it('should show the server time', (done) => {
-      kraken.getTime((error, time) => {
+      kraken.getTime().then((time) => {
         expect(time).to.be.instanceof(Object);
         expect(time).to.have.property('unixtime');
         expect(time).to.have.property('rfc1123');
         done();
-      })
-    })
+      }).catch(error => done(error));
+    });
   });
 
   describe('getAssetInfo', () => {
     it('should show all the assets available on Kraken', (done) => {
-      kraken.getAssetInfo('all', (error, response) => {
+      kraken.getAssetInfo('all').then((response) => {
         expect(response).to.be.instanceof(Object);
         expect(Object.keys(response).length === 0).to.be.false;
         done();
-      })
+      }).catch(error => done(error));
     });
 
     it('should show only two assets on Kraken', (done) => {
-      kraken.getAssetInfo('ETH,XRP', (error, response) => {
+      kraken.getAssetInfo('ETH,XRP').then((response) => {
         expect(response).to.be.instanceof(Object);
         expect(Object.keys(response).length === 2).to.be.true;
         expect(response.XETH).to.have.property('altname');
         done();
-      })
+      }).catch(error=> done(error) );
     });
 
     it('should NOT accept assets that are no strings', (done) => {
-      kraken.getAssetInfo({ curr1: 'ETH', curr2: 'XRP' }, (error, response) => {
+      kraken.getAssetInfo({ curr1: 'ETH', curr2: 'XRP' }).then((response) => {
         expect(response).to.be.undefined;
+        done();
+      }).catch((error) => {
         expect(error).to.be.equal('Assets option must be a string, could be all for all assets or a comma separated values such as ETH,XRP');
         done();
-      })
+      });
     });
 
   });
 
   describe('getTradableAssetPairs', () => {
     it('should show all the tradable assets pairs available on Kraken', (done) => {
-      kraken.getTradableAssetPairs({ info: 'all', pair: 'all' }, (error, response) => {
+      kraken.getTradableAssetPairs({ info: 'all', pair: 'all' }).then((response) => {
         expect(response).to.be.instanceof(Object);
         expect(Object.keys(response).length === 0).to.be.false;
         done();
-      })
+      }).catch(error => done(error));
     });
 
     it('should show only two tradable assets on Kraken', (done) => {
-      kraken.getTradableAssetPairs({ info: 'all', pair: 'ETHUSD,XRPUSD' }, (error, response) => {
+      kraken.getTradableAssetPairs({ info: 'all', pair: 'ETHUSD,XRPUSD' }).then((response) => {
         expect(response).to.be.instanceof(Object);
         expect(Object.keys(response).length === 2).to.be.true;
         expect(response.XETHZUSD).to.have.property('altname');
         done();
-      })
+      }).catch(error => done(error));
     });
   });
 });
